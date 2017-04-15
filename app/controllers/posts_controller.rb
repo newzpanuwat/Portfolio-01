7d2b6c8b22041a71b7a88574d53cae24484f8343
newzpanuwat <pfcreatorit@gmail.com>
2017-04-15 17:58:43 +0700

    initialize portfolio

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,16 +1,25 @@
 class PostsController < ApplicationController
-    def index
-        @posts = Post.all.order("created_at DESC")
-    end
+    before_action :set_post, only: [:show]
 
-    def show
-        @posts = Post.find(params[:id])
-        @posts = Post.order("created_at desc").limit(4).offset(1)
-    end
+	def index
+		@posts = Post.all.order('created_at DESC')
+	end
 
-    private
-    def post_params
-        params.require(:post).permit(:title, :body)
-    end
+	def show	
+
+	end
+
+
+	private
+		def post_params
+			params.require(:post).permit(:title, :body)
+
+		end
+
+        def set_post
+			  @post = Post.find(params[:id])	
+
+        end
+		
+
 end
- 
